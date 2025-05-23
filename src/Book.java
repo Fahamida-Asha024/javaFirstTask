054fda781a666b5c0415b1ec82f4db7fcc201923
Fahamida-Asha024 <168821114+Fahamida-Asha024@users.noreply.github.com>
2025-05-23 19:03:25 +0600

    Save local changes before pulling

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
 
 public class Book {
+    private String title;
+    private String author;
+
+    // Constructor 1: Only title
+    public Book(String title) {
+        this.title = title;
+        this.author = null; // author remains unset
+    }
+
+    // Constructor 2: Title and author
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Method to display book info
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        if (author != null) {
+            System.out.println("Author: " + author);
+        } else {
+            System.out.println("Author: Not specified");
+        }
+    }
 }
+
